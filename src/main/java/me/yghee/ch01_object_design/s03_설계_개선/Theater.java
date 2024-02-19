0626e08b523022583594c4f0f51be8911dfe3ecb
ghyoon93 <gh.yoon93@gmail.com>
2024-02-20 00:53:02 +0900

    [ch01][객체, 설계] : 설계 개선
    - TicketOffice를 TicketSeller 내부로 캡슐화 -> TicketSeller는 TicketOffice를 스스로 관리
    - Bag의 존재를 Audience 내부로 캡슐화 -> Audience는 Bag을 스스로 관리
    -

--- src/main/java/me/yghee/ch01_object_design/s03_설계_개선/Theater.java
+++ src/main/java/me/yghee/ch01_object_design/s03_설계_개선/Theater.java
@@ -12,19 +12,6 @@ public class Theater {
     }
 
     public void enter( Audience audience) {
-        // 관람객의 가방에 초대장이 있으면 판매원이 매표소에서 티켓을 꺼내고,
-        // 소극장은 관람객은 가방에 티켓을 넣는다.
-        if (audience.getBag().hasInvitation()) {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket( ticket );
-        }
-        // 초대장이 없으면 판매원이 매표소에서 티켓을 꺼내고
-        // 소극장은 관람객의 가방에서 티켓 요금만큼 현금을 빼낸 뒤,
-        // 관람객의 가방에 티켓을 넣는다.
-        else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();;
-            audience.getBag().minusAmount( ticket.getFee() );
-            audience.getBag().setTicket( ticket );
-        }
+        ticketSeller.sellTo( audience );
     }
 }
